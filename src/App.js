2d73eed84ebd6d7319500d9dd6e8709b2e700f7a
Divya Sri <divyasriambati123@gmail.com>
2023-06-21 15:32:27 -0400

    add custom font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './fonts/Montserrat-Regular.ttf';
+import './font/Montserrat-Medium.ttf';
+
 function App() {
   return (
     <div className="App">
