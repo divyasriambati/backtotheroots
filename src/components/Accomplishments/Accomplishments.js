c07da43e3ecdde84560c0920225ec79695cb6343
pooja adapa <divyasriambati123@gmail.com>
2023-06-21 15:59:26 -0400

    changes done

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Accomplishments(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Accomplishments;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Navbar.css';
+import facebook from '../../assets/facebook.png';
+import instagram from '../../assets/instagram.png';
+import twitter from '../../assets/twitter.png';
+import youtube from '../../assets/youtube.png';
 
 
 function Navbar(props) {
     return (
         <div className='nav-main-container'>
-            <div className='row-container'>
-                <div className='column1'></div>
+            <div className='nav-row-container'>
+                <div className='nav-column1'>
+                    <p>backtotheroots@gmail.com</p>
+                </div>
+                <div className='nav-column2'>
+                    <img src={facebook}></img>
+                    <img src={twitter}></img>
+                    <img src={youtube}></img>
+                    <img src={instagram}></img>
+                </div>
             </div>
         </div>
     );
