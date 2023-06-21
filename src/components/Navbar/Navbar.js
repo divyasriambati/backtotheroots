2e173eab83a7a0f5602f9b4b922fee8966838326
pooja adapa <divyasriambati123@gmail.com>
2023-06-21 15:43:32 -0400

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+
+function Navbar(props) {
+    return (
+        <div className='nav-main-container'>
+            <div className='row-container'>
+                <div className='column1'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
