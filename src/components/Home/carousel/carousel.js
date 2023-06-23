d6b7400ef187241fd67c485c34c488bbaf5a9467
Rajasekhar_Mutukuri <rajasekhar_muthukuri@capturisk.com>
2023-06-23 13:16:11 -0400

    Added carousel to the home page

--- src/components/Home/carousel/carousel.js
+++ src/components/Home/carousel/carousel.js
@@ -2,32 +2,44 @@ import Slider from 'react-slick'
 import React from 'react';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import { Link } from "react-router-dom";
+import './carousel.css'
+
 export default function Carousel() {
 
   const sliderSettings = {
       dots: true,
+      lazyLoad: true,
       infinite: true,
-      speed: 500,
+      speed: 200,
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoplay: true
+      autoplay: true,
+      initialSlide: 1
     };
 
   return (
-    <div className='content'>
+    <div>
       <Slider {...sliderSettings}>
-        <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
+        
+            <div className='Carousel_section'>
+                <div className='Carousel_components'>
+                    <div className='Carousel_title'>Mission & Vision</div>
+                    <div className='Carousel_description'>To solve India’s grand challenges by bringing together global thought leaders,
+                        entrepreneurs, scientists, policymakers, and grassroots leaders in India.
+                    </div>
+                    <button className="Carousel_button"><Link className='Carousel_link' to="/home"> Donate</Link> </button>
+                </div>
+            </div>
+            <div className='Carousel_section'>
+                <div className='Carousel_components'>
+                    <div className='Carousel_title'>Mission & Vision</div>
+                    <div className='Carousel_description'>To solve India’s grand challenges by bringing together global thought leaders,
+                        entrepreneurs, scientists, policymakers, and grassroots leaders in India.
+                    </div>
+                    <button className="Carousel_button"><Link className='Carousel_link' to="/home"> Donate</Link> </button>
+                </div>
+            </div>
       </Slider>
     </div>
   )
